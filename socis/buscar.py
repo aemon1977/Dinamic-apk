f99ac9e584a3f2b7c13a272aaee5ae9b44ccbf2a
aemon1977 <125026735+aemon1977@users.noreply.github.com>
2024-11-30 14:09:17 +0100

    Delete socis directory

--- socis/buscar.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-app = QApplication([])
-window = QMainWindow()
-uic.loadUi("example.ui", window)
-window.show()
-app.exec_()
